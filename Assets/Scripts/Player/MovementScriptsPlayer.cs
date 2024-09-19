de5b97ff0b236bbc0192bfbf739d3e539d737f9b
DavidJanssonDev <138443239+DavidJanssonDev@users.noreply.github.com>
2024-09-19 13:13:15 +0200

    Comit

---
+++ Assets/Scripts/Player/MovementScriptsPlayer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementScriptsPlayer : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    Vector2 moveDirection = Vector2.zero;
+
+
+    public RICOCHETDUNGEON playerInput;
+
+    public InputAction move;
+    public InputAction fire;
+
+
+    private void Awake()
+    {
+        playerInput = new RICOCHETDUNGEON();
+    }
+
+    private void OnEnable()
+    {
+        move = playerInput.Player.Move;
+        move.Enable();
+
+    }
+
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+
+}
